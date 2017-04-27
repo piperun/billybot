c53182914503d7fa574965fa05ea641e341a7643
piperun <nevadadirren@gmail.com>
2017-04-26 23:30:23 -0700

    made modules work somewhat

--- imports/api/settings/modules.js
+++ imports/api/settings/modules.js
@@ -1,15 +1,27 @@
 import * as modules from '../../modules/bot/'
 
+function execModule(mod) {
+	var modTable = {
+		"Trello": function() {
+			modules.Trello();
+			return 0;
+		}
+	};
 
+	if (typeof modTable[mod] !== 'function') {
+    //throw new Error('Invalid module.');
+    console.log("\x1b[33mWarn:!!Invalid module selected!!\x1b[0m\nPlease make sure you have\nWritten the module correctly!");
+    console.log("Available modules:", modules);
+    return;
+	}
+	modTable[mod]();
+}
 
 export function loadModules() {
 	let mods = Meteor.settings.modules
 
-	
-	console.log(mods);
-
 	for(mod in mods) {
-		console.log(mods[mod]);
+		execModule(mods[mod]);
 	}
 
 }
--- imports/modules/bot/trello/index.js
+++ imports/modules/bot/trello/index.js
@@ -1,3 +1,46 @@
 export default function Trello() {
+	console.log("hello world!");
+	/*
+  // Trello webhook
+  let trelloChannel = Meteor.settings.trello.channelId
 
+  Picker.route('/webhook-trello/', (params, req, res, next) => {
+    let data = ''
+    req.on('error', error => {
+      console.error(error)
+    }).on('data', chunk => {
+      data += chunk
+    }).on('end', () => {
+      req.body = data
+      trelloHandler(req, (error, result) => {
+        if (error) {
+          console.error('Trello: ' + error)
+          res.statusCode = 403
+          res.end('unauthorized\n')
+        } else if (result === 'hook') {
+          // new hook registered, do nothing for now
+        } else if (result) bot.sendMessage(trelloChannel, result)
+        res.end('ok')
+      })
+    })
+  })
+
+  trelloStartup((error, response) => {
+    if (error) console.error('Trello: ' + error)
+    else console.log('Trello: ' + response)
+  })
+
+  // Internal utility functions
+  let getId = (server, nickname) => {
+    let id
+    _.forEach(server.members, (value) => {
+      let nick = server.detailsOf(value.id).nick
+      if (nick === nickname) {
+        id = value.id
+      }
+    })
+    return id || '404'
+  }
+
+*/
 }
